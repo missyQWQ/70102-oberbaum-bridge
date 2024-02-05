2daea458332533e6966eb040cceef925fcadc04d
Yichun Zhang <yz4023@ic.ac.uk>
2024-02-05 14:35:35 +0000

    Modified input data structure and updated readme

--- README.md
+++ README.md
@@ -16,6 +16,35 @@ creatine_results = {
     PID:[AGE, SEX, TEST_DATE, RESULT]
 }
 ```
+## Data Processing
+1. data_combination_pathAndDict(file_path, dict)
+```
+# Input
+1. file_path: The path of history.csv
+2. dict: New patient data, for example,
+creatine_results = {
+    PID:[AGE, SEX, TEST_DATE, RESULT]
+}
+# Output
+DataFrame for the model:
+MRN|age|sex|creatinine_date_0|creatinine_result_0|creatinine_date_1|creatinine_result_1|...
+0  | 18| 'f' | '2024-01-01 06:12:00' |   11.11   | 'yyyy-mm-dd hh:mm:ss' |    12.12    |...
+```
+2. data_combination_dfAndDict(DataFrame, dict)
+```
+# Input
+1. DataFrame: The DataFrame of history data, for example
+MRN|creatinine_date_0|creatinine_result_0|creatinine_date_1|creatinine_result_1|...
+0  | '2024-01-01 06:12:00' |   11.11   | 'yyyy-mm-dd hh:mm:ss' |    12.12    |...
+2. dict: New patient data, for example,
+creatine_results = {
+    PID:[AGE, SEX, TEST_DATE, RESULT]
+}
+# Output
+DataFrame for the model:
+MRN|age|sex|creatinine_date_0|creatinine_result_0|creatinine_date_1|creatinine_result_1|...
+0  | 18| 'f' | '2024-01-01 06:12:00' |   11.11   | 'yyyy-mm-dd hh:mm:ss' |    12.12    |...
+```
 
 ## Input format required for model
 
--- data_processing.py
+++ data_processing.py
@@ -6,17 +6,15 @@ def data_combination_dfAndDict(df, new_data_dict):
     df.insert(loc = 2, column = 'sex', value = pd.NA)
 
     for mrn, record in new_data_dict.items(): # Check patient in the new dataset
-        age = record['age']
-        sex = record['sex']
-        new_creatinine_date = record['creatinine_date']
-        new_creatinine_result = record['creatinine_result']
+        age = record[0]
+        sex = record[1]
+        new_creatinine_date = record[2]
+        new_creatinine_result = record[3]
               
         if mrn in df['mrn'].values: # If this is a old patient already in the records, then update the database
             index = df.index[df['mrn'] == mrn][0]
             df.at[index, 'age'] = age
             df.at[index, 'sex'] = sex
-            # print(pd.isna(df.at[1, 'creatinine_date_1']))
-            # print(pd.isna(df.at[1, 'creatinine_result_1']))
             
             i = 0
             while True: # Look for the first available slot for creatinine data
@@ -34,10 +32,10 @@ def data_combination_dfAndDict(df, new_data_dict):
         else: # If this is a new patient, then add to the database
             new_patient_data = pd.Series({
                 'mrn': mrn,
-                'age': record['age'],
-                'sex': record['sex'],
-                'creatinine_date_0': record['creatinine_date'],
-                'creatinine_result_0': record['creatinine_result']
+                'age': record[0],
+                'sex': record[1],
+                'creatinine_date_0': record[2],
+                'creatinine_result_0': record[3]
             })
             df = pd.concat([df, new_patient_data.to_frame().T], ignore_index = True)
     return df
@@ -50,12 +48,12 @@ def data_combination_pathAndDict(history_data_path, new_data_dict):
 
 def main():
     new_data_dict = {
-        1111: {'age': 30, 'sex': 'm', 'creatinine_date': '2024-01-11 11:11:00', 'creatinine_result': 11.11},
-        2222: {'age': 25, 'sex': 'f', 'creatinine_date': '2024-02-22 22:22:00', 'creatinine_result': 22.22},
-        16318: {'age': 25, 'sex': 'f', 'creatinine_date': '2024-03-30 13:33:00', 'creatinine_result': 33.33}
+        1111: [30, 'm', '2024-01-11 11:11:00', 11.11],
+        2222: [25, 'f', '2024-02-22 22:22:00', 22.22],
+        16318: [25, 'f', '2024-03-30 13:33:00', 33.33]
     }
-    df = data_combination_pathAndDict('history_test.csv', new_data_dict)
-    df.to_csv('updated_data.csv', index = False)
+    df = data_combination_pathAndDict('history.csv', new_data_dict)
+    print(df)
 
 if __name__ == "__main__":
     main()
